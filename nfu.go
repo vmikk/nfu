a36cce351967d8893840cb9c9812e9373ff2d85c
Vladimir Mikryukov <vmikryukov@gmail.com>
2025-04-04 14:49:03 +0300

    init `nfu`

---
+++ nfu.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+// TraceRecord represents a single row from the execution trace file
+type TraceRecord struct {
+	TaskID    string
+	Status    string
+	Realtime  time.Duration
+	CPUPercent float64
+	PeakRSS   string
+	PeakVmem  string
+}
+
